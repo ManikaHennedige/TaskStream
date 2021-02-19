9141b0568ba80e1b3781a8366578bd920f540337
ManikaHennedige <mhennedige@gmail.com>
2021-02-19 17:19:13 +0800

    add stream stuff

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -16,10 +16,13 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
-
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+//        System.out.println("Printing deadlines");
+//        printDeadlines(tasksData);
+//
+//        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+//
+//        printData(tasksData);
+//        printDataWithStreams(tasksData);
         printDeadlinesUsingStreams(tasksData);
         System.out.println("Total number of deadlines using stream");
         System.out.println(countDeadlinesWithStreams(tasksData));
